63f51bd6cab66d08772d12ae9b9a466fedf1ca82
Trihz <stephenmusangi8264@gmail.com>
2023-04-04 23:39:28 +0300

    Homescreen screen

---
+++ lib/HomeScreen/homescreen_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenUI extends StatefulWidget {
+  const HomeScreenUI({super.key});
+
+  @override
+  State<HomeScreenUI> createState() => _HomeScreenUIState();
+}
+
+class _HomeScreenUIState extends State<HomeScreenUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
